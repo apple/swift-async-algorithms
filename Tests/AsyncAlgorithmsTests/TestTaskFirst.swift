beb509d8294d27f93973e7986f74a130c04a4b78
Philippe Hausler <phausler@apple.com>
2022-01-14 08:26:57 -0800

    Fix test build failure with @testable import

--- Tests/AsyncAlgorithmsTests/TestTaskFirst.swift
+++ Tests/AsyncAlgorithmsTests/TestTaskFirst.swift
@@ -10,7 +10,7 @@
 //===----------------------------------------------------------------------===//
 
 import XCTest
-import AsyncAlgorithms
+@testable import AsyncAlgorithms
 
 final class TestTaskFirst: XCTestCase {
   func test_first() async {
