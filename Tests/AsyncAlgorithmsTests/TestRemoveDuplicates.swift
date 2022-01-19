e71fe66e291a24129b824c48f621032b8c79f3fb
Kevin Perry <kperry@apple.com>
2022-01-19 14:49:40 -0800

    Test cleanup

--- Tests/AsyncAlgorithmsTests/TestRemoveDuplicates.swift
+++ Tests/AsyncAlgorithmsTests/TestRemoveDuplicates.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift Async Algorithms open source project
 //
-// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -40,9 +40,7 @@ final class TestRemoveDuplicates: XCTestCase {
     let expected = [1, 2, 3]
     var actual = [Int]()
     let sequence = source.async.removeDuplicates() { prev, next in
-      if next < 0 {
-        throw NSError(domain: NSCocoaErrorDomain, code: -1, userInfo: nil)
-      }
+      let next = try throwOn(-1, next)
       return prev == next
     }
 
@@ -52,7 +50,7 @@ final class TestRemoveDuplicates: XCTestCase {
       }
       XCTFail()
     } catch {
-      XCTAssertEqual((error as NSError).code, -1)
+      XCTAssertEqual(Failure(), error as? Failure)
     }
     XCTAssertEqual(actual, expected)
   }
