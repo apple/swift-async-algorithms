389b1b6805b52866f8e8e8292724be558c3391c8
Philippe Hausler <phausler@apple.com>
2022-01-24 13:33:43 -0800

    Downgrade UnsafeResumption to internal

--- Sources/AsyncAlgorithms/UnsafeResumption.swift
+++ Sources/AsyncAlgorithms/UnsafeResumption.swift
@@ -1,33 +1,33 @@
-public struct UnsafeResumption<Success, Failure: Error> {
+struct UnsafeResumption<Success, Failure: Error> {
   let continuation: UnsafeContinuation<Success, Failure>
   let result: Result<Success, Failure>
   
-  public init(continuation: UnsafeContinuation<Success, Failure>, result: Result<Success, Failure>) {
+  init(continuation: UnsafeContinuation<Success, Failure>, result: Result<Success, Failure>) {
     self.continuation = continuation
     self.result = result
   }
   
-  public init(continuation: UnsafeContinuation<Success, Failure>, success: Success) {
+  init(continuation: UnsafeContinuation<Success, Failure>, success: Success) {
     self.init(continuation: continuation, result: .success(success))
   }
   
-  public init(continuation: UnsafeContinuation<Success, Failure>, failure: Failure) {
+  init(continuation: UnsafeContinuation<Success, Failure>, failure: Failure) {
     self.init(continuation: continuation, result: .failure(failure))
   }
   
-  public func resume() {
+  func resume() {
     continuation.resume(with: result)
   }
 }
 
 extension UnsafeResumption where Failure == Error {
-  public init(continuation: UnsafeContinuation<Success, Failure>, catching body: () throws -> Success) {
+  init(continuation: UnsafeContinuation<Success, Failure>, catching body: () throws -> Success) {
     self.init(continuation: continuation, result: Result(catching: body))
   }
 }
 
 extension UnsafeResumption where Success == Void {
-  public init(continuation: UnsafeContinuation<Success, Failure>) {
+  init(continuation: UnsafeContinuation<Success, Failure>) {
     self.init(continuation: continuation, result: .success(()))
   }
 }
