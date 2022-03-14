6ba2c05cfa3bea8c6a2b27b148f51fd329ce21a0
Philippe Hausler <phausler@apple.com>
2022-03-14 13:41:44 -0700

    A full knobs-and-dials buffering strategy transformation (#61)
    * A full knobs-and-dials buffering strategy transformation
    
    * Refine the AsyncBuffer type to require actor and add some additonal tests for custom buffer types
    
    * Migrate the internal state to an actor
    
    * Remove default unbounded policy from buffers
    
    * Remove some dead code tests and fix space removal to avoid superfluous checks
    
    * Simplification of nominal state machine, which helps eliminate actor interleaving issues. Fixed error/termination handling. Validation diagram tests, using “delay next” operators
    
    * Most robust behavior when multiple tasks are concurrently accessing the iterator
    
    * Removed Envelope, added base iterator Sendable requirement, and added conditional Sendable conformances
    
    * Minor future-proofing
    
    Co-authored-by: Kevin Perry <kperry@apple.com>

---
+++ Sources/AsyncAlgorithms/AsyncBufferSequence.swift
@@ -0,0 +1,277 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Async Algorithms open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+actor AsyncBufferState<Input: Sendable, Output: Sendable> {
+  enum TerminationState: Sendable, CustomStringConvertible {
+    case running
+    case baseFailure(Error) // An error from the base sequence has occurred. We need to process any buffered items before throwing the error. We can rely on it not emitting any more items.
+    case baseTermination
+    case terminal
+
+    var description: String {
+      switch self {
+        case .running: return "running"
+        case .baseFailure: return "base failure"
+        case .baseTermination: return "base termination"
+        case .terminal: return "terminal"
+      }
+    }
+  }
+  
+  var pending = [UnsafeContinuation<Result<Output?, Error>, Never>]()
+  var terminationState = TerminationState.running
+
+  init() { }
+  
+  func drain<Buffer: AsyncBuffer>(buffer: Buffer) async where Buffer.Input == Input, Buffer.Output == Output {
+    guard pending.count > 0 else {
+      return
+    }
+
+    do {
+      if let value = try await buffer.pop() {
+        pending.removeFirst().resume(returning: .success(value))
+      } else {
+        switch terminationState {
+          case .running:
+            // There's no value to report, because it was probably grabbed by next() before we could grab it. The pending continuation was either resumed by next() directly, or will be by a future enqueued value or base termination/failure.
+            break
+          case .baseFailure(let error):
+            // Now that there are no more items in the buffer, we can finally report the base sequence's error and enter terminal state.
+            pending.removeFirst().resume(returning: .failure(error))
+            self.terminate()
+          case .terminal, .baseTermination:
+            self.terminate()
+        }
+      }
+    } catch {
+      // Errors thrown by the buffer immediately terminate the sequence.
+      pending.removeFirst().resume(returning: .failure(error))
+      self.terminate()
+    }
+  }
+
+  func enqueue<Buffer: AsyncBuffer>(_ item: Input, buffer: Buffer) async where Buffer.Input == Input, Buffer.Output == Output {
+    await buffer.push(item)
+    await drain(buffer: buffer)
+  }
+  
+  func fail<Buffer: AsyncBuffer>(_ error: Error, buffer: Buffer) async where Buffer.Input == Input, Buffer.Output == Output {
+    terminationState = .baseFailure(error)
+    await drain(buffer: buffer)
+  }
+  
+  func finish<Buffer: AsyncBuffer>(buffer: Buffer) async where Buffer.Input == Input, Buffer.Output == Output {
+    if case .running = terminationState {
+      terminationState = .baseTermination
+    }
+    await drain(buffer: buffer)
+  }
+
+  func terminate() {
+    terminationState = .terminal
+    pending = []
+    for continuation in pending {
+      continuation.resume(returning: .success(nil))
+    }
+  }
+
+  func next<Buffer: AsyncBuffer>(buffer: Buffer) async throws -> Buffer.Output? where Buffer.Input == Input, Buffer.Output == Output {
+    if case .terminal = terminationState {
+      return nil
+    }
+
+    do {
+      while let value = try await buffer.pop() {
+        if let continuation = pending.first {
+          pending.removeFirst()
+          continuation.resume(returning: .success(value))
+        } else {
+          return value
+        }
+      }
+    } catch {
+      // Errors thrown by the buffer immediately terminate the sequence.
+      self.terminate()
+      throw error
+    }
+
+    switch terminationState {
+      case .running:
+        break
+      case .baseFailure(let error):
+        self.terminate()
+        throw error
+      case .baseTermination, .terminal:
+        self.terminate()
+        return nil
+    }
+
+    let result: Result<Output?, Error> = await withUnsafeContinuation { continuation in
+      pending.append(continuation)
+    }
+    return try result._rethrowGet()
+  }
+}
+
+@rethrows
+public protocol AsyncBuffer: Actor {
+  associatedtype Input: Sendable
+  associatedtype Output: Sendable
+
+  func push(_ element: Input) async
+  func pop() async throws -> Output?
+}
+
+public actor AsyncLimitBuffer<Element: Sendable>: AsyncBuffer {
+  public enum Policy: Sendable {
+    case unbounded
+    case bufferingOldest(Int)
+    case bufferingNewest(Int)
+  }
+  
+  var buffer = [Element]()
+  let policy: Policy
+  
+  init(policy: Policy) {
+    // limits should always be greater than 0 items
+    switch policy {
+      case .bufferingNewest(let limit):
+        precondition(limit > 0)
+      case .bufferingOldest(let limit):
+        precondition(limit > 0)
+      default: break
+    }
+    self.policy = policy
+  }
+  
+  public func push(_ element: Element) async {
+    switch policy {
+    case .unbounded:
+      buffer.append(element)
+    case .bufferingOldest(let limit):
+      if buffer.count < limit {
+        buffer.append(element)
+      }
+    case .bufferingNewest(let limit):
+      if buffer.count < limit {
+        // there is space available
+        buffer.append(element)
+      } else {
+        // no space is available and this should make some room
+        buffer.removeFirst()
+        buffer.append(element)
+      }
+    }
+  }
+  
+  public func pop() async -> Element? {
+    guard buffer.count > 0 else {
+      return nil
+    }
+    return buffer.removeFirst()
+  }
+}
+
+extension AsyncSequence where Element: Sendable {
+  public func buffer<Buffer: AsyncBuffer>(_ createBuffer: @Sendable @escaping () -> Buffer) -> AsyncBufferSequence<Self, Buffer> where Buffer.Input == Element {
+    AsyncBufferSequence(self, createBuffer: createBuffer)
+  }
+  
+  public func buffer(policy limit: AsyncLimitBuffer<Element>.Policy) -> AsyncBufferSequence<Self, AsyncLimitBuffer<Element>> {
+    buffer {
+      AsyncLimitBuffer(policy: limit)
+    }
+  }
+}
+
+public struct AsyncBufferSequence<Base: AsyncSequence, Buffer: AsyncBuffer> where Base.Element == Buffer.Input, Base.AsyncIterator: Sendable {
+  let base: Base
+  let createBuffer: @Sendable () -> Buffer
+  
+  init(_ base: Base, createBuffer: @Sendable @escaping () -> Buffer) {
+    self.base = base
+    self.createBuffer = createBuffer
+  }
+}
+
+extension AsyncBufferSequence: Sendable where Base: Sendable, Base.AsyncIterator: Sendable, Base.Element: Sendable { }
+extension AsyncBufferSequence.Iterator: Sendable where Base: Sendable, Base.AsyncIterator: Sendable, Base.Element: Sendable { }
+
+extension AsyncBufferSequence: AsyncSequence {
+  public typealias Element = Buffer.Output
+  
+  public struct Iterator: AsyncIteratorProtocol {
+    struct Active {
+      var task: Task<Void, Never>?
+      let buffer: Buffer
+      let state: AsyncBufferState<Buffer.Input, Buffer.Output>
+      
+      init(_ iterator: Base.AsyncIterator, buffer: Buffer, state: AsyncBufferState<Buffer.Input, Buffer.Output>) {
+        self.buffer = buffer
+        self.state = state
+        task = Task {
+          var iter = iterator
+          do {
+            while let item = try await iter.next() {
+              await state.enqueue(item, buffer: buffer)
+            }
+            await state.finish(buffer: buffer)
+          } catch {
+            await state.fail(error, buffer: buffer)
+          }
+        }
+      }
+      
+      func next() async rethrows -> Element? {
+        let result: Result<Element?, Error> = await withTaskCancellationHandler {
+          task?.cancel()
+        } operation: {
+          do {
+            let value = try await state.next(buffer: buffer)
+            return .success(value)
+          } catch {
+            task?.cancel()
+            return .failure(error)
+          }
+        }
+        return try result._rethrowGet()
+      }
+    }
+    
+    enum State {
+      case idle(Base.AsyncIterator, @Sendable () -> Buffer)
+      case active(Active)
+    }
+    
+    var state: State
+    
+    init(_ iterator: Base.AsyncIterator, createBuffer: @Sendable @escaping () -> Buffer) {
+      state = .idle(iterator, createBuffer)
+    }
+    
+    public mutating func next() async rethrows -> Element? {
+      switch state {
+      case .idle(let iterator, let createBuffer):
+        let bufferState = AsyncBufferState<Base.Element, Buffer.Output>()
+        let buffer = Active(iterator, buffer: createBuffer(), state: bufferState)
+        state = .active(buffer)
+        return try await buffer.next()
+      case .active(let buffer):
+        return try await buffer.next()
+      }
+    }
+  }
+  
+  public func makeAsyncIterator() -> Iterator {
+    Iterator(base.makeAsyncIterator(), createBuffer: createBuffer)
+  }
+}
---
+++ Tests/AsyncAlgorithmsTests/TestBuffer.swift
@@ -0,0 +1,481 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Async Algorithms open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+import AsyncAlgorithms
+
+final class TestBuffer: XCTestCase {
+  func test_buffering() async {
+    var gated = GatedSequence([1, 2, 3, 4, 5])
+    let sequence = gated.buffer(policy: .unbounded)
+    var iterator = sequence.makeAsyncIterator()
+    
+    gated.advance()
+    var value = await iterator.next()
+    XCTAssertEqual(value, 1)
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    value = await iterator.next()
+    XCTAssertEqual(value, 2)
+    value = await iterator.next()
+    XCTAssertEqual(value, 3)
+    value = await iterator.next()
+    XCTAssertEqual(value, 4)
+    gated.advance()
+    gated.advance()
+    value = await iterator.next()
+    XCTAssertEqual(value, 5)
+    value = await iterator.next()
+    XCTAssertEqual(value, nil)
+    value = await iterator.next()
+    XCTAssertEqual(value, nil)
+  }
+
+  func test_buffering_withError() async {
+    var gated = GatedSequence([1, 2, 3, 4, 5, 6, 7])
+    let gated_map = gated.map { try throwOn(3, $0) }
+    let sequence = gated_map.buffer(policy: .unbounded)
+    var iterator = sequence.makeAsyncIterator()
+
+    gated.advance()
+    var value = try! await iterator.next()
+    XCTAssertEqual(value, 1)
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    value = try! await iterator.next()
+    XCTAssertEqual(value, 2)
+
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    do {
+      value = try await iterator.next()
+      XCTFail("next() should have thrown.")
+    } catch { }
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+  }
+  
+  func test_buffer_delegation() async {
+    actor BufferDelegate: AsyncBuffer {
+      var buffer = [Int]()
+      var pushed = [Int]()
+      
+      func push(_ element: Int) async {
+        buffer.append(element)
+        pushed.append(element)
+      }
+      
+      func pop() async -> Int? {
+        if buffer.count > 0 {
+          return buffer.removeFirst()
+        }
+        return nil
+      }
+    }
+    let delegate = BufferDelegate()
+    var gated = GatedSequence([1, 2, 3, 4, 5])
+    let sequence = gated.buffer {
+      delegate
+    }
+    var iterator = sequence.makeAsyncIterator()
+    
+    gated.advance()
+    var value = await iterator.next()
+    var pushed = await delegate.pushed
+    XCTAssertEqual(pushed, [1])
+    XCTAssertEqual(value, 1)
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    value = await iterator.next()
+    XCTAssertEqual(value, 2)
+    value = await iterator.next()
+    pushed = await delegate.pushed
+    XCTAssertEqual(value, 3)
+    value = await iterator.next()
+    pushed = await delegate.pushed
+    XCTAssertEqual(pushed, [1, 2, 3, 4])
+    XCTAssertEqual(value, 4)
+    gated.advance()
+    gated.advance()
+    value = await iterator.next()
+    pushed = await delegate.pushed
+    XCTAssertEqual(pushed, [1, 2, 3, 4, 5])
+    XCTAssertEqual(value, 5)
+    value = await iterator.next()
+    XCTAssertEqual(value, nil)
+    value = await iterator.next()
+    XCTAssertEqual(value, nil)
+  }
+
+  func test_delegatedBuffer_withError() async {
+    actor BufferDelegate: AsyncBuffer {
+      var buffer = [Int]()
+      var pushed = [Int]()
+
+      func push(_ element: Int) async {
+        buffer.append(element)
+        pushed.append(element)
+      }
+
+      func pop() async throws -> Int? {
+        if buffer.count > 0 {
+          let value = buffer.removeFirst()
+          if value == 3 {
+            throw Failure()
+          }
+          return value
+        }
+        return nil
+      }
+    }
+    let delegate = BufferDelegate()
+
+    var gated = GatedSequence([1, 2, 3, 4, 5, 6, 7])
+    let sequence = gated.buffer { delegate }
+    var iterator = sequence.makeAsyncIterator()
+
+    gated.advance()
+    var value = try! await iterator.next()
+    XCTAssertEqual(value, 1)
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    value = try! await iterator.next()
+    XCTAssertEqual(value, 2)
+
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    gated.advance()
+    do {
+      value = try await iterator.next()
+      XCTFail("next() should have thrown.")
+    } catch { }
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+
+    value = try! await iterator.next()
+    XCTAssertNil(value)
+  }
+  
+  func test_byteBuffer() async {
+    actor ByteBuffer: AsyncBuffer {
+      var buffer: [UInt8]?
+      
+      func push(_ element: UInt8) async {
+        if buffer == nil {
+          buffer = [UInt8]()
+        }
+        buffer?.append(element)
+      }
+      
+      func pop() async -> [UInt8]? {
+        defer { buffer = nil }
+        return buffer
+      }
+    }
+    
+    var data = Data()
+    for _ in 0..<4096 {
+      data.append(UInt8.random(in: 0..<UInt8.max))
+    }
+    let buffered = data.async.buffer {
+      ByteBuffer()
+    }
+    var collected = Data()
+    for await segment in buffered {
+      collected.append(contentsOf: segment)
+    }
+    XCTAssertEqual(data, collected)
+  }
+
+  func test_multi_tasks() async {
+    var values = GatedSequence(Array(0 ... 10))
+    let bufferSeq = values.buffer(policy: .unbounded)
+    var iter_ = bufferSeq.makeAsyncIterator()
+
+    // Initiate the sequence's operation and creation of its Task and actor before sharing its iterator.
+    values.advance()
+    let _ = await iter_.next()
+
+    let iter = iter_
+
+    let task1 = Task<[Int], Never> {
+      var result = [Int]()
+      var iter1 = iter
+      while let val = await iter1.next() {
+        result.append(val)
+      }
+      return result
+    }
+
+    let task2 = Task<[Int], Never> {
+      var result = [Int]()
+      var iter2 = iter
+      while let val = await iter2.next() {
+        result.append(val)
+      }
+      return result
+    }
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+    values.advance()
+
+    let task1Results = await task1.value
+    let task2Results = await task2.value
+
+    XCTAssertEqual(task1Results.sorted(), task1Results)
+    XCTAssertEqual(task2Results.sorted(), task2Results)
+
+    let combined = (task1Results + task2Results).sorted()
+    XCTAssertEqual(combined, Array(1 ... 10))
+  }
+
+  func test_multi_tasks_error() async {
+    var values = GatedSequence(Array(0 ... 10))
+    let mapSeq = values.map { try throwOn(7, $0) }
+    let bufferSeq = mapSeq.buffer(policy: .unbounded)
+    var iter_ = bufferSeq.makeAsyncIterator()
+
+    // Initiate the sequence's operation and creation of its Task and actor before sharing its iterator.
+    values.advance()
+    let _ = try! await iter_.next()
+
+    let iter = iter_
+
+    let task1 = Task<([Int], Error?), Never> {
+      var result = [Int]()
+      var err: Error?
+      var iter1 = iter
+      do {
+        while let val = try await iter1.next() {
+          result.append(val)
+        }
+      } catch {
+        err = error
+      }
+      return (result, err)
+    }
+
+    let task2 = Task<([Int], Error?), Never> {
+      var result = [Int]()
+      var err: Error?
+      var iter2 = iter
+      do {
+        while let val = try await iter2.next() {
+          result.append(val)
+        }
+      } catch {
+        err = error
+      }
+      return (result, err)
+    }
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+
+    let task1Results = await task1.value
+    let task2Results = await task2.value
+
+    XCTAssertEqual(task1Results.0.sorted(), task1Results.0)
+    XCTAssertEqual(task2Results.0.sorted(), task2Results.0)
+
+    let combined = (task1Results.0 + task2Results.0).sorted()
+    XCTAssertEqual(combined, Array(1 ... 6))
+
+    XCTAssertEqual(1, [task1Results, task2Results].compactMap{ $0.1 }.count)
+  }
+
+  func test_multi_tasks_delegateBufferError() async {
+    actor BufferDelegate: AsyncBuffer {
+      var buffer = [Int]()
+      var pushed = [Int]()
+
+      func push(_ element: Int) async {
+        buffer.append(element)
+        pushed.append(element)
+      }
+
+      func pop() async throws -> Int? {
+        if buffer.count > 0 {
+          let value = buffer.removeFirst()
+          if value == 7 {
+            throw Failure()
+          }
+          return value
+        }
+        return nil
+      }
+    }
+    let delegate = BufferDelegate()
+
+    var values = GatedSequence(Array(0 ... 10))
+    let bufferSeq = values.buffer { delegate }
+    var iter_ = bufferSeq.makeAsyncIterator()
+
+    // Initiate the sequence's operation and creation of its Task and actor before sharing its iterator.
+    values.advance()
+    let _ = try! await iter_.next()
+
+    let iter = iter_
+
+    let task1 = Task<([Int], Error?), Never> {
+      var result = [Int]()
+      var err: Error?
+      var iter1 = iter
+      do {
+        while let val = try await iter1.next() {
+          result.append(val)
+        }
+      } catch {
+        err = error
+      }
+      return (result, err)
+    }
+
+    let task2 = Task<([Int], Error?), Never> {
+      var result = [Int]()
+      var err: Error?
+      var iter2 = iter
+      do {
+        while let val = try await iter2.next() {
+          result.append(val)
+        }
+      } catch {
+        err = error
+      }
+      return (result, err)
+    }
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+    values.advance()
+    values.advance()
+
+    try? await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    values.advance()
+
+    let task1Results = await task1.value
+    let task2Results = await task2.value
+
+    XCTAssertEqual(task1Results.0.sorted(), task1Results.0)
+    XCTAssertEqual(task2Results.0.sorted(), task2Results.0)
+
+    let combined = (task1Results.0 + task2Results.0).sorted()
+    XCTAssertEqual(combined, Array(1 ... 6))
+
+    XCTAssertEqual(1, [task1Results, task2Results].compactMap{ $0.1 }.count)
+  }
+
+  func test_bufferingOldest() async {
+    validate {
+      "X-12-   34-    5   |"
+      $0.inputs[0].buffer(policy: .bufferingOldest(2))
+      "X,,,[1,],,[2,],[3,][5,]|"
+    }
+  }
+
+  func test_bufferingOldest_noDrops() async {
+    validate {
+      "X-12   3   4   5   |"
+      $0.inputs[0].buffer(policy: .bufferingOldest(2))
+      "X,,[1,][2,][3,][45]|"
+    }
+  }
+
+  func test_bufferingOldest_error() async {
+    validate {
+      "X-12345^"
+      $0.inputs[0].buffer(policy: .bufferingOldest(2))
+      "X,,,,,,[12^]"
+    }
+  }
+
+  func test_bufferingNewest() async {
+    validate {
+      "X-12-   34    -5|"
+      $0.inputs[0].buffer(policy: .bufferingNewest(2))
+      "X,,,[1,],,[3,],[4,][5,]|"
+    }
+  }
+
+  func test_bufferingNewest_noDrops() async {
+    validate {
+      "X-12   3   4   5   |"
+      $0.inputs[0].buffer(policy: .bufferingNewest(2))
+      "X,,[1,][2,][3,][45]|"
+    }
+  }
+
+  func test_bufferingNewest_error() async {
+    validate {
+      "X-12345^"
+      $0.inputs[0].buffer(policy: .bufferingNewest(2))
+      "X,,,,,,[45^]"
+    }
+  }
+}
