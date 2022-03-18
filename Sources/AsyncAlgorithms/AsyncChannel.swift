b05a1bddb3d6e39a944482b15043dd03ed018345
Philippe Hausler <phausler@apple.com>
2022-03-18 10:25:32 -0700

    Manually line break docs

--- Sources/AsyncAlgorithms/AsyncChannel.swift
+++ Sources/AsyncAlgorithms/AsyncChannel.swift
@@ -11,7 +11,12 @@
 
 /// A channel for sending elements from on task to another with back pressure.
 ///
-/// The `AsyncChannel` class is intended to be used as a communication type between tasks, particularly when one task produces values and another task consumes those values. The back pressure applied by `send(_:)` and `finish()` via the suspension/resume ensure that the production of values does not exceed the consumption of values from iteration. Each of these methods suspends after enqueuing the event and is resumed when the next call to `next()` on the `Iterator` is made.
+/// The `AsyncChannel` class is intended to be used as a communication type between tasks,
+/// particularly when one task produces values and another task consumes those values. The back
+///  pressure applied by `send(_:)` and `finish()` via the suspension/resume ensure that
+///  the production of values does not exceed the consumption of values from iteration. Each of these
+///  methods suspends after enqueuing the event and is resumed when the next call to `next()`
+///  on the `Iterator` is made.
 public final class AsyncChannel<Element: Sendable>: AsyncSequence, Sendable {
   /// The iterator for a `AsyncChannel` instance.
   public struct Iterator: AsyncIteratorProtocol, Sendable {
