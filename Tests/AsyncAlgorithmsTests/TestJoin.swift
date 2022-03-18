d4bdb342839f6a2fc383abe27c0679b7fdfb0605
kperryua <kperry@apple.com>
2022-03-18 14:04:00 -0700

    Add a no-separator variant of joined() to match the stdlib (#87)
    * Add a no-separator variant of joined() to match the stdlib
    
    * @inlinable and @frozen

--- Tests/AsyncAlgorithmsTests/TestJoin.swift
+++ Tests/AsyncAlgorithmsTests/TestJoin.swift
@@ -27,7 +27,7 @@ extension Sequence where Element: Sequence, Element.Element: Sendable {
   }
 }
 
-final class TestJoin: XCTestCase {
+final class TestJoinedBySeparator: XCTestCase {
   func test_join() async {
     let sequences = [[1, 2, 3], [4, 5, 6], [7, 8, 9]].nestedAsync
     var iterator = sequences.joined(separator: [-1, -2, -3].async).makeAsyncIterator()
@@ -107,6 +107,90 @@ final class TestJoin: XCTestCase {
   func test_cancellation() async {
     let source : AsyncLazySequence<[AsyncLazySequence<Indefinite<String>>]> = [Indefinite(value: "test").async].async
     let sequence = source.joined(separator: ["past indefinite"].async)
+    let finished = expectation(description: "finished")
+    let iterated = expectation(description: "iterated")
+    let task = Task {
+      var firstIteration = false
+      for await el in sequence {
+        XCTAssertEqual(el, "test")
+
+        if !firstIteration {
+          firstIteration = true
+          iterated.fulfill()
+        }
+      }
+      finished.fulfill()
+    }
+    // ensure the other task actually starts
+    wait(for: [iterated], timeout: 1.0)
+    // cancellation should ensure the loop finishes
+    // without regards to the remaining underlying sequence
+    task.cancel()
+    wait(for: [finished], timeout: 1.0)
+  }
+}
+
+final class TestJoined: XCTestCase {
+  func test_join() async {
+    let sequences = [[1, 2, 3], [4, 5, 6], [7, 8, 9]].nestedAsync
+    var iterator = sequences.joined().makeAsyncIterator()
+    let expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    var actual = [Int]()
+    while let item = await iterator.next() {
+      actual.append(item)
+    }
+    XCTAssertEqual(expected, actual)
+    let pastEnd = await iterator.next()
+    XCTAssertNil(pastEnd)
+  }
+
+  func test_join_single_sequence() async {
+    let sequences = [[1, 2, 3]].nestedAsync
+    var iterator = sequences.joined().makeAsyncIterator()
+    let expected = [1, 2, 3]
+    var actual = [Int]()
+    while let item = await iterator.next() {
+      actual.append(item)
+    }
+    XCTAssertEqual(expected, actual)
+    let pastEnd = await iterator.next()
+    XCTAssertNil(pastEnd)
+  }
+
+  func test_join_empty() async {
+    let sequences = [AsyncLazySequence<[Int]>]().async
+    var iterator = sequences.joined().makeAsyncIterator()
+    let expected = [Int]()
+    var actual = [Int]()
+    while let item = await iterator.next() {
+      actual.append(item)
+    }
+    XCTAssertEqual(expected, actual)
+    let pastEnd = await iterator.next()
+    XCTAssertNil(pastEnd)
+  }
+
+  func test_throwing() async throws {
+    let sequences = [[1, 2, 3], [4, 5, 6]].nestedAsync(throwsOn: 5)
+    var iterator = sequences.joined().makeAsyncIterator()
+    let expected = [1, 2, 3, 4]
+    var actual = [Int]()
+    do {
+      while let item = try await iterator.next() {
+        actual.append(item)
+      }
+      XCTFail()
+    } catch {
+      XCTAssertEqual(Failure(), error as? Failure)
+    }
+    XCTAssertEqual(expected, actual)
+    let pastEnd = try await iterator.next()
+    XCTAssertNil(pastEnd)
+  }
+
+  func test_cancellation() async {
+    let source : AsyncLazySequence<[AsyncLazySequence<Indefinite<String>>]> = [Indefinite(value: "test").async].async
+    let sequence = source.joined()
     let finished = expectation(description: "finished")
     let iterated = expectation(description: "iterated")
     let task = Task {
