0dc362871882db9ef556d699fbdada23f797eceb
Philippe Hausler <phausler@apple.com>
2022-06-15 15:09:47 -0700

    Use hand coded values instead of defines (only available via Darwin versions of Dispatch)

--- Tests/AsyncAlgorithmsTests/TestTaskSelect.swift
+++ Tests/AsyncAlgorithmsTests/TestTaskSelect.swift
@@ -21,7 +21,7 @@ final class TestTaskSelect: XCTestCase {
       if #available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
         try! await Task.sleep(until: .now + .seconds(2), clock: .continuous)
       } else {
-        try! await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+        try! await Task.sleep(nanoseconds: 2_000_000_000)
       }
       return 2
     }).value
@@ -33,7 +33,7 @@ final class TestTaskSelect: XCTestCase {
       if #available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
         try! await Task.sleep(until: .now + .seconds(2), clock: .continuous)
       } else {
-        try! await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+        try! await Task.sleep(nanoseconds: 2_000_000_000)
       }
       return 1
     }, Task {
@@ -48,7 +48,7 @@ final class TestTaskSelect: XCTestCase {
         if #available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
           try await Task.sleep(until: .now + .seconds(2), clock: .continuous)
         } else {
-          try await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+          try await Task.sleep(nanoseconds: 2_000_000_000)
         }
         return 1
       }, Task { () async throws -> Int in
@@ -74,7 +74,7 @@ final class TestTaskSelect: XCTestCase {
           if #available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
             try? await Task.sleep(until: .now + .seconds(2), clock: .continuous)
           } else {
-            try? await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+            try? await Task.sleep(nanoseconds: 2_000_000_000)
           }
           return 1
         }
@@ -86,7 +86,7 @@ final class TestTaskSelect: XCTestCase {
           if #available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
             try? await Task.sleep(until: .now + .seconds(2), clock: .continuous)
           } else {
-            try? await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+            try? await Task.sleep(nanoseconds: 2_000_000_000)
           }
           return 1
         }
