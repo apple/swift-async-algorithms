3d15a0f2bcacc399dd0889540fc1b7311f31916c
Franz Busch <f.busch@apple.com>
2025-03-28 11:43:34 +0100

    License headers

--- Sources/AsyncAlgorithms/AsyncInclusiveReductionsSequence.swift
+++ Sources/AsyncAlgorithms/AsyncInclusiveReductionsSequence.swift
@@ -1,3 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Async Algorithms open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 extension AsyncSequence {
   /// Returns an asynchronous sequence containing the accumulated results of combining the
   /// elements of the asynchronous sequence using the given closure.
