68e1492c72ba7bc2899e07acebb7323d6a19d154
Philippe Hausler <phausler@apple.com>
2025-09-09 11:03:37 -0700

    yet another stab at 5.10 visionOS guards

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import CompilerPluginSupport
 
 let AsyncAlgorithms_v1_0 = "AvailabilityMacro=AsyncAlgorithms 1.0:macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0"
-#if compiler(>=6.0)
+#if compiler(>=6.0) && swift(>=6.0) // 5.10 doesnt support visionOS availability
 let AsyncAlgorithms_v1_1 =
   "AvailabilityMacro=AsyncAlgorithms 1.1:macOS 15.0, iOS 18.0, tvOS 18.0, watchOS 11.0, visionOS 2.0"
 #else
