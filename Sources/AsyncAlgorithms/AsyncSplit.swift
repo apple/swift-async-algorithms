6663832e1c46033e766376922290ff806a691078
Jonathan Tanner <10051116+aDifferentJT@users.noreply.github.com>
2026-01-17 14:36:34 +0000

    Add header

--- Sources/AsyncAlgorithms/AsyncSplit.swift
+++ Sources/AsyncAlgorithms/AsyncSplit.swift
@@ -1,3 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Async Algorithms open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 import Synchronization
 
 struct AsyncSplitSync<Collection: RangeReplaceableCollection<Base.Element>, Base: AsyncSequence>: AsyncSequence where Base.Element: Equatable {
